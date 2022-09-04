7b8653ca3b02c4b9b90e4875858a5de6400dbee4
Satoshi Kataoka <k.3104is@gmail.com>
2022-09-04 17:31:39 +0900

    init

---
+++ install_snort3_self.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# update and upgrade
+#sudo apt-get update && sudo apt-get dist-upgrade -y
+
+# make store
+# install snort prerequisites
+#sudo apt-get install -y build-essential autotools-dev libdumbnet-dev libluajit-5.1-dev libpcap-dev \
+#     zlib1g-dev pkg-config libhwloc-dev cmake liblzma-dev openssl libssl-dev cpputest libsqlite3-dev \
+#     libtool uuid-dev  git autoconf bison flex libcmocka-dev libnetfilter-queue-dev libunwind-dev \
+#     libmnl-dev ethtool libjemalloc-dev
+
+# safec(for runtime bounds check on certain legacy c-library call)
+cd ~/snort_src
+wget https://github.com/rurban/safeclib/releases/download/v02092020/libsafec-02092020.tar.gz
+tar -xzvf libsafec-02092020.tar.gz
+cd libsafec-02092020.0-g6d921f
+./configure
+make
+sudo make install
+
+# for hyperscan
+# pcre
+cd ~/snort_src/
+wget wget https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.gz
+tar -xzvf pcre-8.45.tar.gz
+cd pcre-8.45
+./configure
+make
+sudo make install
+# gperftool
+cd ~/snort_src
+wget https://github.com/gperftools/gperftools/releases/download/gperftools-2.9.1/gperftools-2.9.1.tar.gz
+tar xzvf gperftools-2.9.1.tar.gz
+cd gperftools-2.9.1
+./configure
+make
+sudo make install
+# ragel
+cd ~/snort_src
+wget http://www.colm.net/files/ragel/ragel-6.10.tar.gz
+tar -xzvf ragel-6.10.tar.gz
+cd ragel-6.10
+./configure
+make
+sudo make install
+# Boost(but don't install)
+cd ~/snort_src
+wget https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.tar.gz
+tar -xvzf boost_1_77_0.tar.gz
+# hyperscan
+cd ~/snort_src
+wget https://github.com/intel/hyperscan/archive/refs/tags/v5.4.0.tar.gz
+tar -xvzf v5.4.0.tar.gz
+mkdir ~/snort_src/hyperscan-5.4.0-build
+cd hyperscan-5.4.0-build/
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DBOOST_ROOT=~/snort_src/boost_1_77_0/ ../hyperscan-5.4.0
+make
+sudo make install
+# flatbuffers
+cd ~/snort_src
+wget https://github.com/google/flatbuffers/archive/refs/tags/v2.0.0.tar.gz -O flatbuffers-v2.0.0.tar.gz
+tar -xzvf flatbuffers-v2.0.0.tar.gz
+mkdir flatbuffers-build
+cd flatbuffers-build
+cmake ../flatbuffers-2.0.0
+make
+sudo make install
+# libdaq
+cd ~/snort_src
+wget https://github.com/snort3/libdaq/archive/refs/tags/v3.0.5.tar.gz -O libdaq-3.0.5.tar.gz
+tar -xzvf libdaq-3.0.5.tar.gz
+cd libdaq-3.0.5
+./bootstrap
+./configure
+make
+sudo make install
+
+# update shared libraries
+sudo ldconfig
+
+# install snort3
+cd ~/snort_src
+wget https://github.com/snort3/snort3/archive/refs/tags/3.1.18.0.tar.gz -O snort3-3.1.18.0.tar.gz
+tar -xzvf snort3-3.1.18.0.tar.gz
+cd snort3-3.1.18.0
+./configure_cmake.sh --prefix=/usr/local --enable-tcmalloc --enable-jemalloc
+cd build
+make
+sudo make install
+
+# check snort3 version
+/usr/local/bin/snort -V
+
+# check default configuration file
+snort -c /usr/local/etc/snort/snort.lua
